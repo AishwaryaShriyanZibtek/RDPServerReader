6a77d3b1d8bb86cf87f629ddd606c9d451a2c6bb
Aishwarya <aishwarya@zibtek.in>
2024-05-14 19:04:02 +0530

    Test server

---
+++ RDPConnectionReader/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace RDPConnectionReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ServerStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(26, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server status: ";
+            // 
+            // ServerStatus
+            // 
+            this.ServerStatus.AutoSize = true;
+            this.ServerStatus.Location = new System.Drawing.Point(130, 58);
+            this.ServerStatus.Name = "ServerStatus";
+            this.ServerStatus.Size = new System.Drawing.Size(12, 15);
+            this.ServerStatus.TabIndex = 1;
+            this.ServerStatus.Text = "-";
+            this.ServerStatus.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 174);
+            this.Controls.Add(this.ServerStatus);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "RDP connection reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ServerStatus;
+    }
+}
+
---
+++ RDPConnectionReader/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Windows.Forms;
+
+namespace RDPConnectionReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("TestServer", PipeDirection.Out))
+            {
+                // Wait for a connection
+                pipeServer.WaitForConnection();
+
+                // Send signal to client
+                using (StreamWriter writer = new StreamWriter(pipeServer))
+                {
+                    ServerStatus.Text = "Server started";
+                    writer.WriteLine("ServerStarted");
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
